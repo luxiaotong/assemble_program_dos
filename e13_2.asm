fcc22f39a4f0c6b61c2e69fce0c7e47a42a481f0
luxiaotong <luxiaotong713@sina.com>
2022-04-14 15:51:52 +0800

    expirement 13.2

---
+++ e13_2.asm
@@ -0,0 +1,47 @@
+assume cs:codesg
+
+codesg segment
+start:
+    mov ax, cs
+    mov ds, ax
+    mov si, offset lp
+    mov ax, 0
+    mov es, ax
+    mov di, 200h
+    mov cx, offset lpend - offset lp
+    cld
+    rep movsb
+
+    mov ax, 0
+    mov es, ax
+    mov word ptr es:[7ch*4], 200h
+    mov word ptr es:[7ch*4+2], 0
+    
+    mov ax, 0b800h
+    mov es, ax
+    mov di, 160*12
+
+    mov bx, offset s - offset se
+    mov cx, 80
+s:
+    mov byte ptr es:[di], '!'
+    add di, 2
+    int 7ch
+se:
+    nop
+    mov ax, 4c00h
+    int 21h
+lp:
+    push bp
+    mov bp, sp
+    dec cx
+    jcxz lpret
+    add [bp+2], bx
+lpret:
+    pop bp
+    iret
+lpend:
+    nop
+codesg ends
+
+end start
